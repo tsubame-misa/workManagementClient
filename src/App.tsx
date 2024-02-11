94c72a9001d801b9b994e05515a1884cea49d982
Misato Watanabe <watamisa080513@gmail.com>
2024-02-12 00:28:28 +0900

    ルーティング大枠

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Project from "./pages/Project";
+import User from "./pages/User";
 import "bulma/css/bulma.css";
 
-import "./App.css";
-
-function App() {
-  return <></>;
+function Header() {
+  return (
+    <section className="hero is-warning">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">header</h1>
+        </div>
+      </div>
+    </section>
+  );
 }
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <section className="section has-background-warning-light">
+        <div className="container">
+          {/* <div className="block has-text-right">
+            <button className="button is-warning is-inverted is-outlined">
+              ログイン
+            </button>
+          </div> */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user/:userId" element={<User />} />
+            <Route
+              path="/user/:userId/project/:projectId"
+              element={<Project />}
+            />
+          </Routes>
+        </div>
+      </section>
+    </BrowserRouter>
+  );
+}
 export default App;
