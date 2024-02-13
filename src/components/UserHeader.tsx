d8e6d566d58a2b07d647c8a9608d7da774ef5f33
Misato Watanabe <watamisa080513@gmail.com>
2024-02-13 13:28:01 +0900

    userHeaderレイアウト調整

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import "./UserHeader.css";
 
 type Props = {
   user: user;
@@ -11,8 +12,9 @@ const UserHeader = ({ user }: Props) => {
 
   return (
     <div>
-      <div className="user">
+      <div className="user_header_info section pt-0">
         <img className="user_icon" src={user.icon ? user.icon : "/vite.svg"} />
+        <div className="user_name">{user.user_name}</div>
       </div>
 
       <div className="tabs is-centered">
